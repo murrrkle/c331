4742a1ecde8103d6d8d14de9f3d14799cf72ad5b
Michael Hung <michaelhung@shaw.ca>
2013-12-02 16:07:29 -0700

    update

--- Assignment 4/ArrayUtils.java
+++ Assignment 4/ArrayUtils.java
@@ -4,24 +4,56 @@ import cpsc331.assignment4.Array;
 
 public class ArrayUtils<T extends Comparable<T>> {
     
-    private void quicksort(Array<T> A)
+    private void quicksort(Array<T> A, int left, int right) 
     {
+        if (right <= left) 
+            return;
         
+        int i = partition(A, left, right);
+        quicksort(A, left, i-1);
+        quicksort(A, i+1, right);
     }
     
     private int partition(Array<T> A, int left, int right)
-    {
-        return 0;
+    {   
+        int i = left - 1;
+        int j = right;
+        
+        while (true)
+        {
+            while(i < j)
+            {
+                while(j > i)
+                {
+                    if (j == left)
+                    {
+                        break;
+                    }
+                j--;
+                }
+                
+            if (i >= j)
+                {break;}
+                
+            i++;
+                
+            }
+        
+        exchange(A, i, j);
+        return i;
+        }
     }
     
     private void exchange(Array<T> A, int i, int j)
     {
-        
+        T swap = A.get(i);
+        A.set(i, A.get(j));
+        A.set(j,  swap);
     }
     
     public void sort(Array<T> A )
     {    
-        
+        quicksort(A, 0, A.length() - 1);
     };
 
 }
